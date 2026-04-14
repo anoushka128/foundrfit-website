cc61e817a3f4194890ab645af4a1e1f86f460162
anoushka128 <bharwanianoushka@gmail.com>
2026-04-13 23:36:17 -0400

    Remove unused app routes for landing page deploy

--- app/onboarding/page.tsx
+++
@@ -1,5 +0,0 @@
-import { OnboardingForm } from "@/components/onboarding/onboarding-form";
-
-export default function OnboardingPage() {
-  return <OnboardingForm />;
-}
--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/components/auth/signup-form";
-
-export default function SignupPage() {
-  return (
-    <main className="mx-auto flex min-h-screen max-w-md items-center px-4">
-      <SignupForm />
-    </main>
-  );
-}
